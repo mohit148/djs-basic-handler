49e32c51df24041608b110f7c9351d2520a551a6
mohit148 <116991297+mohit148@users.noreply.github.com>
2023-05-05 09:49:57 +0000

    Added a emojis.json to use emojis faster without copy and pasting each emoji id. Just use ${emotes.<emoteName>}

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
+const emotes = require('../../../emojis.json');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply(`Pong! ${emotes.wave}`);
 	},
 };
