1cbe3e73ae12d01b4c690ced3b488cf5c7fb2f25
mohit148 <116991297+mohit148@users.noreply.github.com>
2023-05-07 11:15:49 +0000

    Test

--- commands/test commands/rank.js
+++ commands/test commands/rank.js
@@ -5,17 +5,18 @@ module.exports = {
     .setName('rank')
     .setDescription('shows users rank card!'),
   async execute(interaction) {
-    let name = interaction.user.tag;
+    const name = interaction.user.tag;
     if (name.length > 10) {
-      name = name.substring(0, 10) + "...";
+      name = name.substring(0, 11) + "...";
     }
     const embed = new EmbedBuilder()
       .setColor(2829617)
       .addFields(
-        { name: name, value: '\`Level 32\` :star: **|** \`XP 289/750\` :sparkles:' }
+        { name: name, value: '\`Level 32\` :star: **|** \`Rank #14\`\n[=============                 ]:triangular_flag_on_post:' }
       )
-      .setThumbnail(interaction.user.avatarURL());
+      .setThumbnail(interaction.user.displayAvatarURL({dynamic: true}));
 
     await interaction.reply({ embeds: [embed] });
+    await interaction.followUp({content: ':bulb: **Did you know?** \n You can check the reward list for level roles using </rewards:28040282029>', ephemeral: true});
   },
 };
